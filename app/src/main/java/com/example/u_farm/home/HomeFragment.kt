c751771e421fb85326ea41c221b654fa3c2e2897
Priyadharshini0101 <priyadharshinis5102@gmail.com>
2022-04-17 15:05:06 +0530

    Base

--- app/src/main/java/com/example/u_farm/home/HomeFragment.kt
+++ app/src/main/java/com/example/u_farm/home/HomeFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.u_farm.R
+import com.example.u_farm.databinding.FragmentAddProblemsBinding
+import com.example.u_farm.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
@@ -14,6 +17,8 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val binding:FragmentHomeBinding=DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false)
+
+        return binding.root
     }
 }
